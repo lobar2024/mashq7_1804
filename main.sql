19fb75105e77e9bdfeed94c87520fddb6770a338
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-04-18 09:46:40 +0500

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER
+);
+
+INSERT INTO users (name, age)
+VALUES 
+('Ali', 20),
+('Vali', 17),
+('Sami', 25);
+
+UPDATE users
+SET age = age + 1;
+
+SELECT * FROM users;
